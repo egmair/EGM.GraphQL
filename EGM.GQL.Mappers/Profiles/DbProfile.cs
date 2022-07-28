f7ae99fd9bf6001a1e663f3939b248f31890925d
euanm <euangmair@gmail.com>
2022-07-28 22:43:50 +0100

    Created mappers project.

---
+++ EGM.GQL.Mappers/Profiles/DbProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EGM.GQL.DataAccess.Primitives.Entities;
+using EGM.GQL.Primitives.Models;
+
+namespace EGM.GQL.Mappers.Profiles
+{
+    public class DbProfile : Profile
+    {
+        public DbProfile()
+        {
+            CreateMap<DbPerson, Person>();
+            CreateMap<Person, DbPerson>();
+            CreateMap<DbSex, Sex>();
+            CreateMap<Sex, DbSex>();
+        }
+    }
+}
